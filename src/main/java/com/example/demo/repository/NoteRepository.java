4ca75f50c2a4d21d2dd9f25095831bd9ec8c6455
pet339 <petra.pristyak@gmail.com>
2021-05-30 21:00:35 +0200

    clean

--- src/main/java/com/example/demo/repository/NoteRepository.java
+++ src/main/java/com/example/demo/repository/NoteRepository.java
@@ -1,43 +1,9 @@
 package com.example.demo.repository;
 
-
 import com.example.demo.domain.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-
-
-/*
-    @PersistenceContext
-    private EntityManager em;
-
-    @Transactional
-    public Note save(Note feedback) {
-        return em.merge(feedback);
-    }
-
-    public List<Note> findAll() {
-        return em.createQuery("SELECT n FROM Note n", Note.class).getResultList();
-    }
-
-    public Note findById(long id) {
-        return em.find(Note.class, id);
-    }
-
-    public List<Note> findByKeyword(String keyword) {
-        return em.createQuery("SELECT n FROM Note n WHERE n.text LIKE ?1", Note.class).setParameter(1, '%' + keyword + '%').getResultList();
-    }
-
-    @Transactional
-    public void deleteById(long id) {
-        Note todo = findById(id);
-        em.remove(todo);
-    }*/
 }
